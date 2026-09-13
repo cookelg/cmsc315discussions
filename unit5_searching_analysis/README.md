2caae41a23443d2ab6f1e75682e38a14ab9287fe
Lawrence Cooke <lawrence.cooke7@gmail.com>
2026-09-13 18:49:50 -0400

    unit 5 complete

--- unit5_searching_analysis/README.md
+++ unit5_searching_analysis/README.md
@@ -22,10 +22,28 @@ This assignment compares linear search and binary search.
 
 ## Discussion Board Reflection
 
-After completing the programming assignment, add this reflection to your initial discussion post in LEO.
+1. **What concepts or skills did you learn while completing this assignment?**
+This was a great opportunity to continue to practice recursion. Additionally, 
+implementing a search on a large dataset gave me an opportunity to research and 
+learn about memory constraints; I was worried that I would create a list that was 
+too large to be contained in memory, and it turns out that a list of ten million 
+indices is handled without any issues. It is when the dataset size approaches the 
+amount of RAM for the computer the program is running on where this would become an 
+issue. Going down this rabbit hole led me to learn how cases like this are handled:
+the python libraries Dask and Polars can be used in cases like this.
 
-Your reflection should be approximately 150–200 words and address the following questions:
+2. **What challenges did you encounter, and how did you overcome them?**
+I initially ran into an issue converting the csv to a list. In my initial attempt,
+I simply cast the csv reader object to a list. Instead of strings like I wanted, 
+each line of the csv file was cast to its own individual list, giving me a list of 
+lists each list containing the email. I instead had to iterate the csv with a for
+loop and individually append the strings to the python list. 
 
-1. What concepts or skills did you learn while completing this assignment?
-2. What challenges did you encounter, and how did you overcome them?
-3. Explain when to use linear versus binary search, including tradeoffs in real-world scenarios.
+3. **Explain when to use linear versus binary search, including tradeoffs in real-world scenarios.**
+Linear searches would be more efficient than binary searches in the case of small 
+datasets. Linear search is more dynamic than binary search because it can search 
+through an unsorted list. For example, if I'm implementing a program that searches 
+for employee information for a company that only has a small number of employees, 
+or if I am implementing a program that needs to search through an unsorted dataset.
+A binary search becomes more useful as the number of objects increases, assuming the
+objects in the list are sorted. 
--- unit5_searching_analysis/unit5_discussion.py
+++ unit5_searching_analysis/unit5_discussion.py
@@ -14,6 +14,7 @@ and program output.
 """
 import random
 import time
+import csv
 
 
 def linear_search(lst, target) -> int:
@@ -28,6 +29,10 @@ def linear_search(lst, target) -> int:
     - Add comments explaining why linear search
       has O(n) time complexity.
     """
+    # in a linear search, every value in the list is checked against the target 
+    # until a match is found. The worst case is that the value does not exist, 
+    # meaning the function checks every index. Therefore, this function has a 
+    # time complexity of O(N)
     for i, value in enumerate(lst):
         if target == value:
             return i
@@ -47,17 +52,29 @@ def binary_search(lst, target) -> int:
     - Add comments explaining how each iteration
       reduces the search space.
     """
+    # I chose to use recurstion to implement the Binary Search. This was done using 
+    # a helper function.
     return binary_search_recursive(lst, 0, len(lst) - 1, target)
 
 
-def binary_search_recursive(lst: list, low: int, high: int, target) -> int:
+def binary_search_recursive(lst: list, low: int, high: int, target: int) -> int:
+    # base case: if low is greater then high, the target is not in list
     if low > high:
         return -1
+    # during each recursion, a new mid value is calculated. This will be the index
+    # that is compared against the target.
     mid = int((low + high) / 2)
+    # If the value at index mix is greater than the target, mid -1 is passed as 
+    # the new high index in the next recursion, thereby only passing half of the 
+    # list. 
     if lst[mid] > target:
         return binary_search_recursive(lst, low, mid - 1, target)
+    # If the value at the mid index is less than the target, mid + 1 is passed as 
+    # the new low index in the next recursion, again only passing half of the list.
     elif lst[mid] < target:
         return binary_search_recursive(lst, mid + 1, high, target)
+    # if all the above tests return false, then the only other case is that the
+    # value at index mid equals the target, and that index is returned.
     else:
         return mid
 
@@ -80,13 +97,25 @@ def main():
     print("\n=== SMALL DATASET TEST ===")
     print("TODO: Create a small dataset and test both searches.")
 
+    # the small dataset is a sorted list of 25 random integers ranging from 1-100.
     lst = sorted(random.sample(range(1, 101), 25))
     print(lst)
+
+    # the starting time is recorded
     start_time = time.perf_counter()
+
+    # Linear Search is about as efficient as Binary Search with small datasets
+    # because the number of checks is only slightly more than a Binary Search.
     print(f"7 found at index {linear_search(lst, 7)}")
+
+    # the end time is captured after the process is run
     end_time = time.perf_counter()
+    # the total time is calculated by subtracting start time - end time. The
+    # output is in seconds
     total_time_sec = end_time - start_time
+    #converting seconds to milliseconds
     total_time_milli = total_time_sec * 1000
+
     print(f"Linear search time elapsed: {total_time_milli:.6f} milliseconds")
     print(f"Linear time elapsed: {total_time_sec:.6f} seconds")
     start_time = time.perf_counter()
@@ -94,6 +123,8 @@ def main():
     end_time = time.perf_counter()
     total_time_sec = end_time - start_time
     total_time_milli = total_time_sec * 1000
+
+    # There is no noticeable time difference between linear and binary search
     print(f"Binary search time elapsed: {total_time_milli:.6f} milliseconds")
     print(f"Binary search time elapsed: {total_time_sec:.6f} seconds")
 
@@ -111,22 +142,45 @@ def main():
     print("\n=== LARGE DATASET TEST ===")
     print("TODO: Create a larger dataset and compare results.")
 
-    lst = sorted(random.sample(range(1, 30001), 10000))
-
     start_time = time.perf_counter()
-    print(f"9876 found at index {linear_search(lst, 9876)}")
+    # the large dataset is a sorted list of 10,000,000 integers ranging in value
+    # between 1-30,000,000.
+    lst = sorted(random.sample(range(1, 30000001), 10000000))
+    end_time = time.perf_counter()
+
+    total_time_sec = end_time - start_time
+    total_time_milli = total_time_sec * 1000
+    # first time measurement is the total elapsed time taken to sort the large 
+    # dataset.
+    print("Time elapsed to generate and sort a list of 10,000,000 integers:")
+    print(f"-- {total_time_milli:.6f} milliseconds")
+    print(f"-- {total_time_sec:.6f} seconds\n")
+
+    # Capture the start time for the linear search
+    start_time = time.perf_counter()
+    print(f"9,986,500 found at index {linear_search(lst, 9986500)}")
     end_time = time.perf_counter()
     total_time_sec = end_time - start_time
     total_time_milli = total_time_sec * 1000
+
+    # Searching for a large value guaruntees that a linear search will take longer
+    # to parse. In the case of a large dataset, a linear search takes noticably 
+    # longer.
     print(f"Linear search time elapsed: {total_time_milli:.6f} milliseconds")
     print(f"Linear time elapsed: {total_time_sec:.6f} seconds")
+
+    # Capture the start time for the binary search
     start_time = time.perf_counter()
-    print(f"9876 found at index {binary_search(lst, 9876)}")
+
+    # Even for larger values, a binary search takes significantly less time than 
+    # linear search. 
+    print(f"9,986,500 found at index {binary_search(lst, 9986500)}")
     end_time = time.perf_counter()
     total_time_sec = end_time - start_time
     total_time_milli = total_time_sec * 1000
     print(f"Binary search time elapsed: {total_time_milli:.6f} milliseconds")
     print(f"Binary search time elapsed: {total_time_sec:.6f} seconds")
+
     # ===============================
     # TODO (Student): EDGE CASES
     # ===============================
@@ -144,6 +198,57 @@ def main():
 
     print("\n=== EDGE CASE TESTS ===")
     print("TODO: Demonstrate and explain edge cases.")
+    
+    # Edge case: search for a string rather than an integer.
+    # This edge case also serves as a real-life use case. AI was used to generate 
+    # a sorted CSV file with 200 unique emails stored in emails.csv
+
+    email_list = []
+    # open emails.csv and create a csv.DictReader object. Then, iterate through 
+    # the 'email' column and append the strings to a list.
+    with open('emails.csv', mode='r', newline='', encoding='utf-8') as csv_file:
+        reader = csv.DictReader(csv_file)
+
+        for row in reader:
+            email_list.append(row['email'])
+    
+    start_time = time.perf_counter()
+
+    # This string is an email that is located near the end of the csv file.
+    new_email = "z0shzbuk3@protonmail.com"
+
+    # binary_search is called, and the index of the email is stored in a variable
+    email_index = binary_search(email_list, new_email)
+
+    # if the email exists, a print statement gives the index of that email
+    if email_index > -1:
+        print(f"{new_email} located at index {email_index}")
+    # if the email does not exits, a message indecates that the email is available
+    else:
+        print(f"{new_email} is available for use!")
+    end_time = time.perf_counter()
+    total_time_sec = end_time - start_time
+    total_time_milli = total_time_sec * 1000
+    print(f"Binary search time elapsed: {total_time_milli:.6f} milliseconds")
+    print(f"Binary search time elapsed: {total_time_sec:.6f} seconds")
+
+    start_time = time.perf_counter()
+
+    # an email that is not in the list is passed to binary_search
+    new_email = "lawrence@gmail.com"
+    email_index = binary_search(email_list, new_email)
+    if email_index > -1:
+        print(f"{new_email} located at index {email_index}")
+    else:
+        print(f"{new_email} is available for use!")
+    end_time = time.perf_counter()
+    total_time_sec = end_time - start_time
+    total_time_milli = total_time_sec * 1000
+    print(f"Binary search time elapsed: {total_time_milli:.6f} milliseconds")
+    print(f"Binary search time elapsed: {total_time_sec:.6f} seconds")
+
+
+
 
 
 if __name__ == "__main__":
